36eb49ac777ffdca962259a6dffab225ee3b0e62
Yannic Meyer <anoderay@users.noreply.github.com>
2023-07-19 13:59:07 +0200

    Update email_att_dl.py

--- email_att_dl.py
+++ email_att_dl.py
@@ -21,10 +21,19 @@ except:
 # get all attachments for each email from INBOX folder
 with MailBox(host).login(user, secret) as mailbox:
     for msg in mailbox.fetch():
+        att_count = len(msg.attachments)    #count attachments to decide how to proceed
+        att_index = 1
+        msg_title = ''.join(e for e in msg.subject if e.isalnum() or e.isspace()) # sanitize msg title for later use
         for att in msg.attachments:
-            logging.info("downloaded " + att.filename, att.content_type)
-            with open(out_dir + "/" + att.filename, 'wb') as f: 
-                f.write(att.payload)
+            att_name, att_ext = os.path.splitext(att.filename)
+            if att_count == 1:
+                with open(out_dir + "/" + msg_title + att_ext, 'wb') as f: 
+                    f.write(att.payload)
+            elif att_count > 1:
+                with open(out_dir + "/" + msg_title + " "+ str(att_index) + att_ext, 'wb') as f: 
+                    f.write(att.payload)
+                    att_index = att_index + 1
+            else:
+                logging.debug("this email didnt include an attachment")
         msg_processed.append(msg.uid) #collect the processed emails
-    logging.info("deleting processed messages") 
     mailbox.delete(msg_processed) #deleting the processed emails
